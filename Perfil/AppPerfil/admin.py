965be3882d1f24581d5e9ea9f15b5780e7d1f3d3
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-13 00:41:08 -0500

    admin register

--- Perfil/AppPerfil/admin.py
+++ Perfil/AppPerfil/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id_carta','games', 'sub', 'credits')
+
+admin.site.register(Profile,ProfileAdmin)
--- Perfil/AppPerfil/models.py
+++ Perfil/AppPerfil/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Profile(models.Model):
-    user = models.CharField(max_length=100)
+    user = models.CharField(max_length=100, unique=True)
     id_carta = models.CharField(max_length=24)
     games = models.IntegerField()
     sub = models.BooleanField(default=False)
+    credits = models.IntegerField(default=0)
     
