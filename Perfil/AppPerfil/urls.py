9f2b66411ddd7685dd316708193b640b844cc7a6
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-25 00:55:48 -0500

    Almacenar cartas con http

--- Perfil/AppPerfil/urls.py
+++ Perfil/AppPerfil/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('perfil/<str:user>/', ProfileView.as_view(), name="perfil"),
     path('inventario/<str:user>/', UserInventoryView.as_view(), name="inventario"),
+    path('agg_inventario/', AddInventary.as_view(), name="AGGinventario"),
     path('inventario2/<str:user>/', InventoryView.as_view(), name="inventario2"),
 ]
--- Perfil/AppPerfil/views.py
+++ Perfil/AppPerfil/views.py
@@ -4,6 +4,7 @@ from .serializers import ProfileSerializer,UserInventorySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
+from django.db import transaction
 
 class ProfileView(APIView):
     def get(self,request,user):
@@ -22,3 +23,35 @@ class InventoryView(APIView):
         profile = UserInventory.objects.filter(user=user)
         serializer = UserInventorySerializer(profile,many=True)
         return Response(serializer.data)
+
+class AddInventary(APIView):
+    def post(self,request):
+        username = request.data.get('user')
+        order_id = request.data.get('order_id')
+        
+        #api_url = f'https://store.thenexusbattles2.cloud/webserver/obtener-orden/{order_id}'
+        api_url = f'http://localhost:3000/obtener-orden/{order_id}'
+        response = requests.get(api_url)
+        
+        if response.status_code == 200:
+            order_data = response.json()
+            items = order_data.get('Items',[])
+            with transaction.atomic():
+                for item in items:
+                    id_carta = item.get('id_carta')
+                    
+                    try:
+                        card = UserInventory.objects.get(user=username,id_carta=id_carta)
+                        card.quantity +=1
+                        card.save()
+                    except UserInventory.DoesNotExist:
+                        response = requests.get('https://cards.thenexusbattles2.cloud/api/all/')
+                        data = response.json()
+                        #iterar por el json
+                        for carta in data:
+                            #comparar el id del json con el id que pasamos
+                            if carta.get('_id') == id_carta:
+                                coleccion = carta.get('coleccion')
+                        UserInventory.objects.create(user=username,id_carta=id_carta,type=coleccion)
+                return Response({'Ok:':'Se almacenaron todos los cambios en el inventario'})
+            
